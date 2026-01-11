6eb67d23733bbad6b4c9ba4638735df14682dd94
tbeaumont79 <tbeaumont79@icloud.com>
2026-01-11 12:43:48 +0100

    feat: add the entrypoint of the plugin

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./core/types";
+export * from "./core/featureFlagManager";
+export * from "./core/factory";
+export * from "./vue/plugin";
+export * from "./vue/useFeatureFlags";
+export * from "./vue/constants";
