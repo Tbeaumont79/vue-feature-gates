958e66b1aa5cbe32446593d3f8bec4624f41aa67
tbeaumont79 <tbeaumont79@icloud.com>
2026-01-27 20:03:24 +0100

    refactor: update core imports and remove old files
    - Update imports in core/createFeatureFlagController.ts
    - Update imports in core/factory.ts
    - Remove deprecated src/core/types.ts
    - Remove deprecated src/vue/* files (moved to new structure)

--- src/core/factory.ts
+++ src/core/factory.ts
@@ -1,5 +1,5 @@
 import { createFeatureFlagController } from "./createFeatureFlagController";
-import type { FeatureFlagsShape } from "./types";
+import type { FeatureFlagsShape } from "../types";
 
 export function createFeatureFlagManager<T extends FeatureFlagsShape>(
 	flags: T,
